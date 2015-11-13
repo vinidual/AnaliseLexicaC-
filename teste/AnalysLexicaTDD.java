f63d178d79217cca689fca59ad0bbe98ece94698
mush8 <mushroom5kk@gmail.com>
2015-11-13 14:47:04 -0200

    tenter refazer

---
+++ teste/AnalysLexicaTDD.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class AnalysLexicaTDD {
+	FileCm file;
+	
+	@Test
+	public void fileInexistent() {
+		file = new FileCm("file");
+		assertFalse(file.exists());
+	}	
+	
+	@Test
+	public void fileExist() {
+		file = new FileCm("src/testSuccess.cm");
+		assertTrue(file.exists());
+	}
+	
+	@Test
+	public void fileExtensionInvalid() {
+		file = new FileCm("src/test.txt");
+		assertTrue(file.exists());
+		assertFalse(file.verifyExtension());
+		assertFalse(file.valid());
+	}
+	
+	@Test
+	public void fileExtensionValid() {
+		file = new FileCm("src/testSuccess.cm");
+		assertTrue(file.valid());
+	}
+	
+	@Test
+	public void convertCharValid(){
+		char []expecteds = {'1','2','3',';',10};
+		file = new FileCm("src/testNumValid.cm");
+		assertArrayEquals(expecteds, file.convertVectorChar());
+	}
+	
+}
